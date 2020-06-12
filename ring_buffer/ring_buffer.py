4a7435467ec881237f4912e5e823d894144c4528
Rachele Edwards <dot.com1472@gmail.com>
2020-06-12 09:58:54 -0600

    almost passing all tests in buffer, stuck at adding multiple elements

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,26 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.size = 0
 
     def append(self, item):
-        pass
+        if self.size is self.capacity:
+            print("TRIGGERING IF")
+            # self.storage.pop(0)
+            # self.storage.insert(0, item)
+            self.storage.reverse()
+            print(self.storage)
+            self.storage.pop()
+            print(self.storage)
+            self.storage.append(item)
+            print(self.storage)
+            self.storage.reverse()
+            print(self.storage)
+        else:
+            print("HITTING ELSE")
+            self.storage.append(item)
+            self.size += 1
 
     def get(self):
-        pass
+        return self.storage
