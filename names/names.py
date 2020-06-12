7152a5bda0cc4e32765b82a7b5e8ffb8e3e1fe41
Rachele Edwards <dot.com1472@gmail.com>
2020-06-12 10:32:39 -0600

    tried implementing queue/stack or binary tree and not understanding logic. Going back to square one

--- names/names.py
+++ names/names.py
@@ -11,8 +11,7 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
-first = []
-second = []
+names = []
 
 # Replace the nested for loops below with your improvements
 # for name_1 in names_1:
@@ -21,16 +20,10 @@ second = []
 #             duplicates.append(name_1)
 
 for name_1 in names_1:
-    first.append(name_1)
-
-for name_2 in names_2:
-    second.append(name_2)
-
-for one in first:
-    for two in second:
-        if one == two:
-            duplicates.append(one)
-
+    
+    for name_2 in names_2:
+        if name_1 == name_2:
+            duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
